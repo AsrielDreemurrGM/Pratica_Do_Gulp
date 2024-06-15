6b5024dc251955227b4689a34410e4533c420a99
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-06-14 21:57:48 -0300

    Instalado Gulp Uglify (Comprimir JavaScript)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
 
 function compilaSass() {
     return gulp.src('./source/styles/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./build/styles'));
 }
+
