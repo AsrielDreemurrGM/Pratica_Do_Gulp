709c0fda589cd4ecd8672f8cdf588275d6897014
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-06-14 21:53:09 -0300

    Criado Função compilaSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+    return gulp.src('./source/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'));
+}
